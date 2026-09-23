065c4739a0fa7d14421029ec85b4d534a8cb7f39
Pu Gao <161672165+FelWint3r@users.noreply.github.com>
2026-09-23 10:54:55 +0800

    Delete find_non_reference_BC200_in_human_population directory

--- find_non_reference_BC200_in_human_population/workflow_find_BC200_in_human_population.sh
+++
@@ -1,4 +0,0 @@
-#gunzip shapeit5-phased-callset_final-vcf.phased.vcf.gz
-python3 vcf2fasta.py >vcf.fasta &&
-makeblastdb  -dbtype nucl -out db -in vcf.fasta &&
-blastn -task blastn -query Human_BC200_gene.fasta  -out find_non_reference_BC200.txt -db db -evalue 1e-30
